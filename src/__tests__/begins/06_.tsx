743ab33f58e1e42837d01fa95dfc76945dd05826
vietdan1613 <viet.dan.1613@gmail.com>
2025-10-23 20:29:51 +0700

    update react testing

--- src/__tests__/begins/06_.tsx
+++ src/__tests__/begins/06_.tsx
@@ -1,11 +1,53 @@
-// import Location from '../../examples/location'
-// import { render, screen, waitFor } from '@testing-library/react'
-// import { vi } from 'vitest'
+import Location from '../../examples/location'
+import { render, screen, waitFor } from '@testing-library/react'
+import { vi } from 'vitest'
+beforeAll(() => {
+    // @ts-expect-error -- just for test ---
+    global.navigator.geolocation = {
+        getCurrentPosition: vi.fn(),
+        watchPosition: vi.fn(),
+        clearWatch: vi.fn(),
+    };
+})
+
+afterEach(() => {
+    vi.clearAllMocks();
+});
 
 test('displays the users current location', async () => {
+    const fakeGeolocation = {
+        latitude: 35,
+        longitude: 139,
+    };
 
+    // @ts-expect-error -- just for test ---
+    global.navigator.geolocation.getCurrentPosition.mockImplementationOnce((success) =>
+        success({
+            coords: {
+                latitude: fakeGeolocation.latitude,
+                longitude: fakeGeolocation.longitude,
+            },
+        }),
+    )
+
+    render(<Location />)
+
+    await waitFor(() => {
+        expect(screen.getByText(/latitude/i)).toHaveTextContent('Latitude: 35')
+        expect(screen.getByText(/longitude/i)).toHaveTextContent('Longitude: 139')
+    })
 })
 
 test('displays loading state when geolocation is not available yet', async () => {
+    // @ts-expect-error -- just for test ---
+    global.navigator.geolocation.getCurrentPosition.mockImplementationOnce((success, error) =>
+        error({
+            code: 1,
+            message: 'User denied Geolocation',
+        }),
+    )
 
+    render(<Location />)
+
+    expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
 })
