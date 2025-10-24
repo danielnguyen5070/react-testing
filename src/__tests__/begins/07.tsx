52935c20eb4b573cc171c84fb52fffaee559cc07
vietdan1613 <viet.dan.1613@gmail.com>
2025-10-24 14:37:07 +0700

    update react testing

--- src/__tests__/begins/07.tsx
+++ src/__tests__/begins/07.tsx
@@ -1,5 +1,6 @@
 // import EasyButton from "../../components/easy-button"
 // import { render, screen } from "@testing-library/react"
+// import { ThemeProvider, type Theme } from "../../components/theme"
 
 test('renders with light theme styles', () => {
 })
